97114ca3071990bc9145e9c5258e4872f6599fe2
ANINJURIES <98586371+ANINJURIES@users.noreply.github.com>
2023-09-22 16:38:11 +0530

    Add files via upload

---
+++ TypeConversion.py
@@ -0,0 +1,8 @@
+# Taking input from the user interactively
+user_input = input("Please enter Value 1 : ")
+user_input_2 = input("Please enter Value 2 : ")
+
+# Printing the sum
+print("Sum :", float(user_input) + float(user_input_2))
+
+# if you remove float type casting then input function has taken string input it will append string not perform addition 
